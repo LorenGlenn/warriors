8832b98caf596464b476cccb90f860e5df477b2b
Loren Glenn <lorencglenn@gmail.com>
2017-01-26 17:57:29 -0800

    add player-detail component with dynamic routing

--- src/app/roster/roster.component.ts
+++ src/app/roster/roster.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Player } from '../player.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-roster',
@@ -7,10 +8,15 @@ import { Player } from '../player.model';
   styleUrls: ['./roster.component.css']
 })
 export class RosterComponent {
+  constructor(private router: Router){}
 
   players: Player[] = [
    new Player("Stephen Curry", "PG", "6'3", 180, 28, 1),
    new Player("Klay Thompson", "SG", "6'7", 200, 26, 2)
  ];
+
+ goToDetailPage(selected: Player) {
+  this.router.navigate(['roster', selected.id]);
+};
 
 }
