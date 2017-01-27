719748b23cf8495b7b5de17419864e6324afcea9
Loren Glenn <lorencglenn@gmail.com>
2017-01-26 17:50:04 -0800

    create player object

---
+++ src/app/player.model.ts
@@ -0,0 +1,3 @@
+export class Player {
+  constructor (public name: string, public position: string, public height: string, public weight: number, public age: number, public id: number) { }
+}
--- src/app/roster/roster.component.ts
+++ src/app/roster/roster.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Player } from '../player.model';
 
 @Component({
   selector: 'app-roster',
   templateUrl: './roster.component.html',
   styleUrls: ['./roster.component.css']
 })
-export class RosterComponent implements OnInit {
+export class RosterComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  players: Player[] = [
+   new Player("Stephen Curry", "PG", "6'3", 180, 28, 1),
+   new Player("Klay Thompson", "SG", "6'7", 200, 26, 2)
+ ];
 
 }
